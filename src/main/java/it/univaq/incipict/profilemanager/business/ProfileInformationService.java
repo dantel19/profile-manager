5f5b79e9f936c49f3592b500cdf2d6c6a996b699
dantel19 <danieletellina@gmail.com>
2017-02-05 02:04:01 +0100

    Finalize Services and relative implementations #28

---
+++ src/main/java/it/univaq/incipict/profilemanager/business/ProfileInformationService.java
@@ -0,0 +1,28 @@
+/*
+ * Profile Manager - Copyright (C) 2016  Daniele Tellina
+ *
+ * Profile Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * Profile Manager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with Profile Manager.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package it.univaq.incipict.profilemanager.business;
+
+import it.univaq.incipict.profilemanager.business.model.ProfileInformation;
+
+/**
+ * 
+ * @author Daniele Tellina
+ *
+ */
+public interface ProfileInformationService extends CRUDService<Long, ProfileInformation>{
+
+}
